ce4d069f6ce4b62c5bdd11aa6f545fc21a10c3a5
Julia Cruz <juliacruz97@hotmail.com>
2025-10-03 02:18:05 +0100

    01

--- Data from yfinance.py
+++ Data from yfinance.py
@@ -1,3 +1,10 @@
 # Problema 1: Dados do yfinance:
+#  Using the yfinance Python package, write a function called get_data() that downloads all hourly data for the previous five days for the five FAANG stocks:
+# Facebook (META)
+# Apple (AAPL)
+# Amazon (AMZN)
+# Netflix (NFLX)
+# Google (GOOG)
 
-print ('Ola GitHub')
+
+
