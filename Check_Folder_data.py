1d77a4bba22f926d9acbb5acad768711c3fa460b
Julia Cruz <juliacruz97@hotmail.com>
2025-12-19 11:49:03 +0000

    25

--- Check_Folder_data.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-data_folder = 'data/' # path to the data folder
-
-if os.path.exists(data_folder): # Check if it exists
-    print ("the 'data'folder exists!")
-else:
-    print ("the 'data'folder does not exists!")
